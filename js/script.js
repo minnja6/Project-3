350a41171e8aedc77ee65713b1609ed2f3a2b325
minnja6 <jasmine.minniweather@verizon.com>
2019-06-26 13:20:29 -0500

    added change event for other
    added change event for other

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
+//Making sure the document is ready to run intial code
 $(document).ready(function(){
-  $('#name').focus();
+    //Selecting the "name" input element and placing focus on it
+    $('#name').focus();
+    //Using a descendant selector to select the "Select Theme" 'option' element and hide it 
+    $('#design option:first-Child').hide();
 });
-$('#other-title').hide();
+
+//Targetting the "Other" input field and hiding it initially with JS only
+// $('#other-title').hide();
+
+//change function
+$('#title').on('change', function(e){
+    //Creating a conditional statement that will show the text field only when 'other' is chosen
+    if($(e.target).val() == 'other'){
+        $('#other-title').show();
+    } else {
+        //Hiding the text field otherwise
+        $('#other-title').hide();
+    };
+});
+$('#title').trigger('change');
+
+
+
+    
