6a4db7f96ea719e1185aedbb0917129c56ead27a
minnja6 <jasmine.minniweather@verizon.com>
2019-07-01 10:21:45 -0500

    upadtes
    Adding checkboxes and disabling some

--- js/script.js
+++ js/script.js
@@ -19,20 +19,66 @@ $('#title').on('change', function(e){
         $('#other-title').hide();
     };
 });
-//Updating the color field to read "Please select a T-shirt Theme"
+//Updating the color field to read "Please select a T-shirt Theme until a design is chosen"
 $('#color').prepend('<option selected>Please select a T-shirt Theme</option>');
 //Hiding the colors in the "Color" drop down menu
 $('#color option').hide();
-//using regular expression to hide/show shirt colors depending on the design
+//using regular expression to hide/show shirt colors depending on the design chosen
 $('#design').on('change', function(){
     //creating a variable to hold the text of the selected design
     const design = $('#design option:selected').text();
     //creating a variable to hold the reg ex match of the two designs
     const regEx = /Theme - (.*)$/;
-    //creating a variable to hold the first matches content
+    //creating a variable to hold the first matches content and regex
     const matched = design.match(regEx)[1];
     //hiding and showing both matches based on which one is selected 
     $(`#color option:contains("${matched}")`).show();
     $(`#color option:not(:contains("${matched}"))`).hide();
     });
+
+//creating a variable to store the total activity costs and initially setting it to 0
+let totalCost = 0;
+//selecting "activities" by it's class
+const activities = document.querySelector('.activities');
+//adding an event listener to listen for changes in the checkboxes
+$('.activities').addEventListener('change', () => {
+    //selecting the input of the checkboxes
+    const checkbox = document.querySelectorAll('input[type = "checkbox"]');
+
+    if(checkbox[1].checked){
+        checkbox[3].prop('disabled', true);
+    } else {
+        checkbox[3].prop('disabled', false);
+    }
+    if(checkbox[3].checked){
+        checkbox[1].prop('disabled', true);
+    } else {
+        checkbox[1].prop('disabled', false);   
+    }
+    if(checkbox[2].checked){
+        checkbox[4].prop('disabled', true);
+        checkbox[6].prop('disabled', true);
+    } else {
+        checkbox[4].prop('disabled', false);
+        checkbox[6].prop('disabled', false);
+    }
+    if(checkbox[4].checked){
+        checkbox[2].prop('disabled', true);
+        checkbox[6].prop('disabled', true);
+    } else {
+        checkbox[2].prop('disabled', false);
+        checkbox[6].prop('disabled', false);  
+    }
+    if(checkbox[6].checked){
+        checkbox[2].prop('disabled', true);
+        checkbox[4].prop('disabled', true);
+    } else {
+        checkbox[2].prop('disabled', false);
+        checkbox[4].prop('disabled', false);  
+});
+
+
+
     
+
+
